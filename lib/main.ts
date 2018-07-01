fe61032f71ab9ed6ed4b439a20ed8470a52d19d9
Mgen <m@mgenware.com>
2018-07-01 17:51:48 +0800

    Upgrade binaryInsertSearch to support compareFn argument

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,22 @@
-export function binaryInsertSearch<T, V>(array: T[], target: V, keyFn: (item: T) => V) {
+export function binaryInsertSearch<T>(
+  array: T[],
+  target: T,
+  compareFn: (a: T, b: T) => number,
+) {
+  if (!Array.isArray(array)) {
+    throw new Error(`The argument "array" is not an array, got: ${array}`);
+  }
+  if (!compareFn) {
+    throw new Error('The argument "compareFn" cannot be empty');
+  }
+
   let i = 0;
   let j = array.length;
 
   while (i <= j) {
     const mid = Math.floor((i + j) / 2);
-    const value = keyFn(array[mid]);
-    if (value < target) {
+    const result = compareFn(array[mid], target);
+    if (result < 0) {
       i = mid + 1;
     } else {
       j = mid - 1;
--- tests/binaryInsertSearch.test.js
+++ tests/binaryInsertSearch.test.js
@@ -1,13 +1,26 @@
 const { binaryInsertSearch } = require('..');
 
+const PrimitiveCompareFn = (a, b) => {
+  return a - b;
+};
+
+const PrimitiveDescendingCompareFn = (a, b) => {
+  return b - a;
+};
+
 test('Existent value', () => {
-  expect(binaryInsertSearch([1, 2, 3], 2, i => i)).toBe(1);
-  expect(binaryInsertSearch([-1], -1, i => i)).toBe(0);
+  expect(binaryInsertSearch([1, 2, 3], 2, PrimitiveCompareFn)).toBe(1);
+  expect(binaryInsertSearch([-1], -1, PrimitiveCompareFn)).toBe(0);
 });
 
 test('Non-existent value', () => {
-  expect(binaryInsertSearch([1, 4, 5], 2, i => i)).toBe(1);
-  expect(binaryInsertSearch([1, 4, 5], 10, i => i)).toBe(3);
-  expect(binaryInsertSearch([-1], -4, i => i)).toBe(0);
-  expect(binaryInsertSearch([-1], 4, i => i)).toBe(1);
+  expect(binaryInsertSearch([1, 4, 5], 2, PrimitiveCompareFn)).toBe(1);
+  expect(binaryInsertSearch([1, 4, 5], 10, PrimitiveCompareFn)).toBe(3);
+  expect(binaryInsertSearch([-1], -4, PrimitiveCompareFn)).toBe(0);
+  expect(binaryInsertSearch([-1], 4, PrimitiveCompareFn)).toBe(1);
+  expect(binaryInsertSearch([], -9, PrimitiveCompareFn)).toBe(0);
+});
+
+test('Descending order', () => {
+  expect(binaryInsertSearch([1, 2, 3], 15, PrimitiveDescendingCompareFn)).toBe(0);
 });
